d3756f86bff928b183c0be6724b26d4441363151
umitAydin3455 <umitaydinn3455@gmail.com>
2024-04-03 23:26:25 +0300

    güncelleme3
    Uygulama güncelleştirmesi gerçekleşti

---
+++ vizeOdevi/KitapForm.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.XPath;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace vizeOdevi
+{
+    public partial class KitapForm : Form
+    {
+        private List<Book> books = new List<Book>();
+        private string jsonFilePath = Path.Combine(Application.StartupPath, "books.json");
+        private DateTime kitapCikisTarih;
+
+        public KitapForm()
+        {
+            InitializeComponent();
+            LoadBooksFromJson();
+        }
+        private void LoadBooksFromJson()
+        {
+            if (File.Exists(jsonFilePath))
+            {
+                string json = File.ReadAllText(jsonFilePath);
+                books = JsonConvert.DeserializeObject<List<Book>>(json);
+                RefreshDataGridView();
+            }
+        }
+        private void SaveBooksToJson()
+        {
+            string json = JsonConvert.SerializeObject(books);
+            File.WriteAllText(jsonFilePath, json);
+        }
+        private void btnKitapEkle_Click(object sender, EventArgs e)
+        {
+            string kitapAd = kitapAdi.Text;
+            string kitapYazar = kitapYazari.Text;
+            string yayin = yayini.Text;
+            string KitapCikisTarih = kitapCikisTarihi.Text;
+
+
+            Book newBook = new Book(kitapAd, kitapYazar, yayin, kitapCikisTarih);
+            books.Add(newBook);
+            SaveBooksToJson();
+            RefreshDataGridView();
+            kitapAdi.Text = "";
+            kitapYazari.Text = "";
+            yayini.Text = "";
+            kitapCikisTarihi.Text = "";
+
+            MessageBox.Show("Kitap başarıyla kaydedildi.");
+        }
+        private void RefreshDataGridView()
+        {
+            dGVKitapListesi.DataSource = null;
+            dGVKitapListesi.DataSource = books;
+        }
+
+        private void bntKitapSil_Click(object sender, EventArgs e)
+        {
+            if (dGVKitapListesi.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dGVKitapListesi.SelectedRows[0];
+                Book selectedBook = (Book)selectedRow.DataBoundItem;
+                books.Remove(selectedBook);
+                SaveBooksToJson();
+                RefreshDataGridView();
+                MessageBox.Show("Seçilen kitap başarıyla silindi.");
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek istediğiniz kitabı seçin.");
+            }
+        }
+        //-----------------------------
+        private Book selectedBook;
+        private void btnKitapSec_Click(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kitap formunu aç ve seçimi bekle
+            KitapForm kitapForm = new KitapForm();
+            if (kitapForm.ShowDialog() == DialogResult.OK)
+            {
+                selectedBook = kitapForm.SelectedBook;
+            }
+        }
+        //-----------------------------
+    }
+    public class Book
+    {
+        public string KitapAd { get; set; }
+        public string KitapYazar { get; set; }
+        public string yayin { get; set; }
+        public DateTime KitapCikisTarih { get; set; }
+
+        public Book(string kitapAdi, string kitapYazari, string yayini, DateTime kitapCikisTarihi)
+        {
+            KitapAd = kitapAdi;
+            KitapYazar = kitapYazari;
+            yayin = yayini;
+            KitapCikisTarih = kitapCikisTarihi;
+        }
+    }
+}
---
+++ vizeOdevi/UyeForm.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vizeOdevi;
+
+namespace vizeOdevi
+{
+    public partial class UyeForm : Form
+    {
+        private List<Member> members = new List<Member>();
+        private string jsonFilePath = Path.Combine(Application.StartupPath, "members.json");
+
+        public UyeForm()
+        {
+            InitializeComponent();
+            LoadMembersFromJson();
+        }
+
+        private void LoadMembersFromJson()
+        {
+            if (File.Exists(jsonFilePath))
+            {
+                string json = File.ReadAllText(jsonFilePath);
+                members = JsonConvert.DeserializeObject<List<Member>>(json);
+                RefreshDataGridView();
+            }
+        }
+
+        private void SaveMembersToJson()
+        {
+            string json = JsonConvert.SerializeObject(members);
+            File.WriteAllText(jsonFilePath, json);
+        }
+
+        private void ekle_Click(object sender, EventArgs e)
+        {
+            string ad = adTextBox.Text;
+            string soyad = soyadTextBox.Text;
+            string telno = telnoTextBox.Text;
+            string email = emailTextBox.Text;
+
+            Member newMember = new Member(ad, soyad, telno, email);
+            members.Add(newMember);
+            SaveMembersToJson();
+            RefreshDataGridView();
+            MessageBox.Show("Üye başarıyla kaydedildi.");
+        }
+
+        private void RefreshDataGridView()
+        {
+            dGVÜyeListesi.DataSource = null;
+            dGVÜyeListesi.DataSource = members;
+        }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            if (dGVÜyeListesi.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dGVÜyeListesi.SelectedRows[0];
+                Member selectedMember = (Member)selectedRow.DataBoundItem;
+                members.Remove(selectedMember);
+                SaveMembersToJson();
+                RefreshDataGridView();
+                MessageBox.Show("Seçilen üye başarıyla silindi.");
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek istediğiniz üyeyi seçin.");
+            }
+        }
+        private Member selectedMember;
+        //-----------------------------
+        private void btnUyeSec_Click(object sender, EventArgs e)
+        {
+            // Üye formunu aç ve seçimi bekle
+            UyeForm uyeForm = new UyeForm();
+            if (uyeForm.ShowDialog() == DialogResult.OK)
+            {
+                selectedMember = uyeForm.SelectedMember;
+            }
+        }
+        //-----------------------------
+    }
+
+    public class Member
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Telno { get; set; }
+        public string Email { get; set; }
+
+        public Member(string ad, string soyad, string telno, string email)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Telno = telno;
+            Email = email;
+        }
+    }
+}
